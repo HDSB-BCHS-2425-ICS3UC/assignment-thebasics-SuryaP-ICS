41004e292c681bb11c96f648fba97f221b1c043d
SuryaP-ICS <1premkumasur@hdsb.ca>
2025-02-26 14:24:20 -0500

    Powers, Squares and Modulus
    Added those operations

--- Math Operations.py
+++ Math Operations.py
@@ -16,3 +16,12 @@ print("the quotient after dividing the integers 21 and 7 is:", quot)
 # Quotient for a float
 quot_float= 32.0/5
 print("The quoteient after dividing the number 32 by 5 is:", quot_float)
+# Square root
+Square= math.sqrt(9)
+print("The square root of 9 is:", Square)
+# Exponent
+nth_power= 3**7
+print("3 to the power of 7 is:", nth_power)
+# Modulus
+mod1= 82%70
+print("82 percent of 70 is:", mod1)
