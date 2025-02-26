5d3e1a73acd0402b8c4415b379d11fe82d16ff1d
SuryaP-ICS <1premkumasur@hdsb.ca>
2025-02-26 14:23:12 -0500

    Quot & Quot Float
    Added quot and quot float lines

--- Math Operations.py
+++ Math Operations.py
@@ -10,3 +10,9 @@ print("Here is 21-3:", difference)
 # Product
 prod= 3*7*2
 print("Here is the product of multiplying 3 by 7 and then by 2:", prod)
+# Quotient for an integer
+quot= 21/7
+print("the quotient after dividing the integers 21 and 7 is:", quot)
+# Quotient for a float
+quot_float= 32.0/5
+print("The quoteient after dividing the number 32 by 5 is:", quot_float)
