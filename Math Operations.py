577b32c7929b46d5284c2dd8b70e40964e7b648b
SuryaP-ICS <1premkumasur@hdsb.ca>
2025-02-26 14:22:29 -0500

    Subtraction and Product
    Added subtraction and product lines

--- Math Operations.py
+++ Math Operations.py
@@ -4,3 +4,9 @@ import math
 # Addition
 sum_add = 21+3
 print("Here is 21+3:", sum_add)
+#Subtraction
+difference= 21-3
+print("Here is 21-3:", difference)
+# Product
+prod= 3*7*2
+print("Here is the product of multiplying 3 by 7 and then by 2:", prod)
