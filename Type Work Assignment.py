1b29e4abd5ec911e6f9aeef4329399a11d4600fe
SuryaP-ICS <1premkumasur@hdsb.ca>
2025-02-26 14:31:18 -0500

    Added string and boolean
    explanatory titles are key

--- Type Work Assignment.py
+++ Type Work Assignment.py
@@ -2,3 +2,7 @@
 #Declare what the variables are for each type
 int_variable = 42   #integer
 float_variable = 3.14 #float
+str_variable = ("hello world") #string
+boolean_variable = True #boolean
+
+
