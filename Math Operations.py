819e821e99d41e12dfc645440711fd2e2af897ec
SuryaP-ICS <1premkumasur@hdsb.ca>
2025-02-26 14:21:01 -0500

    Addition
    Added the addition operation

---
+++ Math Operations.py
@@ -0,0 +1,6 @@
+import math
+
+
+# Addition
+sum_add = 21+3
+print("Here is 21+3:", sum_add)
---
+++ Type Work Assignment.py
@@ -0,0 +1,12 @@
+#Type Work Assignment
+#Declare what the variables are for each type 
+int_variable = 42   #integer
+float_variable = 3.14 #float
+str_variable = ("hello world") #string
+boolean_variable = True #boolean
+
+#Print Variables
+print("integer variable:",int_variable)
+print("float variable:",float_variable)
+print("string variable:",str_variable)
+print("boolean variable:",boolean_variable)
