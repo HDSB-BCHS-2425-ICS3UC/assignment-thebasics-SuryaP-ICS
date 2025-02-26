9895f64c4f5618365bc25d0dd34f139de59459f5
SuryaP-ICS <1premkumasur@hdsb.ca>
2025-02-26 14:32:44 -0500

    Added string and variable print statements
    om

--- Type Work Assignment.py
+++ Type Work Assignment.py
@@ -8,5 +8,8 @@ boolean_variable = True #boolean
 #Print Variables
 print("integer variable:",int_variable)
 print("float variable:",float_variable)
+print("string variable:",str_variable)
+print("boolean variable:",boolean_variable)
+
 
 
