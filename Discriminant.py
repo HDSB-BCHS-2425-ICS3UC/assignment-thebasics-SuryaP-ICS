634e317c6fc430ef1fac4b2d76511efcea2efff3
SuryaP-ICS <1premkumasur@hdsb.ca>
2025-02-27 13:55:05 -0500

    Added B and c values

--- Discriminant.py
+++ Discriminant.py
@@ -3,3 +3,7 @@ import math
 # define value a
 a = int(input("Type A value here:"))
 print("this is your A value:", a) # A value stored
+b = int(input("Type your B value:"))
+print("this is your B value:", b) # B value stored
+c = int(input("Enter C value here:"))
+print("this is your C value:", c) # c value stored
