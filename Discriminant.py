37fb0ab39f591904fc962d0d4d175ef2a6faf423
SuryaP-ICS <1premkumasur@hdsb.ca>
2025-02-27 13:56:25 -0500

    Added final calculations for b^2, -4ac and full thing

--- Discriminant.py
+++ Discriminant.py
@@ -7,3 +7,10 @@ b = int(input("Type your B value:"))
 print("this is your B value:", b) # B value stored
 c = int(input("Enter C value here:"))
 print("this is your C value:", c) # c value stored
+
+om = b**2 # Variable to represent the b^2
+prod = -4*a*c # Variable to represent the -4ac
+difference = om - prod
+print("This is the discriminant:", difference)
+
+
